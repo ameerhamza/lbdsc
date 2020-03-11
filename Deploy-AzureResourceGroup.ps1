35fff50b1f8197c06393fe1ee5083dfa35a9fe31
a.hamza <ameerhamza@gmail.com>
2020-03-11 23:40:41 +0400

    asdf

--- Deploy-AzureResourceGroup.ps1
+++ Deploy-AzureResourceGroup.ps1
@@ -1,26 +1,26 @@
-#region login to Azure
+# #region login to Azure
 
-$rgName = 'automationrg'
-$autoaccountName = 'autoaccount'
-$connectionName = "AzureRunAsConnection"
+# $rgName = 'automationrg'
+# $autoaccountName = 'autoaccount'
+# $connectionName = "AzureRunAsConnection"
 
-$VerbosePreference = "Continue"
-$conn = Get-AutomationConnection `  -Name $connectionName 
+# $VerbosePreference = "Continue"
+# $conn = Get-AutomationConnection `  -Name $connectionName 
 
 
-Write-Verbose $("[*]Logging in to Azure...")
+# Write-Verbose $("[*]Logging in to Azure...")
   
-$connectionResult = Connect-AzAccount `
-    -TenantId $conn.TenantID `
-    -ApplicationId $conn.ApplicationID `
-    -CertificateThumbprint $conn.CertificateThumbprint `
-    -ServicePrincipal
+# $connectionResult = Connect-AzAccount `
+#     -TenantId $conn.TenantID `
+#     -ApplicationId $conn.ApplicationID `
+#     -CertificateThumbprint $conn.CertificateThumbprint `
+#     -ServicePrincipal
 
-Write-Verbose "[*]Logged in."
+# Write-Verbose "[*]Logged in."
 
-$subscription = Get-AzSubscription -SubscriptionId $conn.SubscriptionId
-Set-AzContext -Subscription $subscription
-#endregion
+# $subscription = Get-AzSubscription -SubscriptionId $conn.SubscriptionId
+# Set-AzContext -Subscription $subscription
+# #endregion
 
 Write-Verbose "[*]Creating Resource Group"
 $rg = New-AzResourceGroup -Name lbrg -Location eastus -Force
@@ -28,8 +28,8 @@ $rg = New-AzResourceGroup -Name lbrg -Location eastus -Force
 Write-Verbose "[*]Creating Resource Group Deployment"
 
 New-AzResourceGroupDeployment -Name "az-deploy-01" -ResourceGroupName $rg.ResourceGroupName -Location $rg.Location `
-    -TemplateFile "https://raw.githubusercontent.com/ameerhamza/lbdsc/master/network.json"  `
-    -TemplateParameterFile "https://raw.githubusercontent.com/ameerhamza/lbdsc/master/network.parameters.json"
+    -TemplateUri "https://raw.githubusercontent.com/ameerhamza/lbdsc/master/network.json"  `
+    -TemplateParameterUri "https://raw.githubusercontent.com/ameerhamza/lbdsc/master/network.parameters.json"
 
 $vnet = 'vnet'
 $lbsubnet = 'lbsubnet'
